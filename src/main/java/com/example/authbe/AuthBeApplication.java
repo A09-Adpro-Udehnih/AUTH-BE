b4ad725909c430e7e291b167909af53c6c220696
Tristan A. <tristanagraa@gmail.com>
2025-03-28 18:21:41 +0700

    chore: init springboot project

---
+++ src/main/java/com/example/authbe/AuthBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.authbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthBeApplication.class, args);
+	}
+
+}
