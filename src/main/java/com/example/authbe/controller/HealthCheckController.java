91ce664db20b01b04375586eaad8fba56164ea1a
TrayMachi <tristan.agra@ui.ac.id>
2025-05-22 05:50:16 +0700

    feat: use string instead so it can be showed in web

--- src/main/java/com/example/authbe/controller/HealthCheckController.java
+++ src/main/java/com/example/authbe/controller/HealthCheckController.java
@@ -1,22 +1,14 @@
 package com.example.authbe.controller;
 
-import com.example.authbe.dto.GlobalResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpStatus;
 
 @RestController
 public class HealthCheckController {
     
     @GetMapping("/")
-    public ResponseEntity<GlobalResponse<String>> healthCheck() {
-        GlobalResponse<String> response = GlobalResponse.<String>builder()
-                .code(HttpStatus.OK)
-                .success(true)
-                .message("Service is healthy")
-                .data("Auth Service is running")
-                .build();
-        return ResponseEntity.ok(response);
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("Auth Service is running"); 
     }
 }
