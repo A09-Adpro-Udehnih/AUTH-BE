803ec6e3bfac9c583c324346f0fb537f6653ac98
TrayMachi <tristan.agra@ui.ac.id>
2025-05-22 13:47:42 +0700

    feat: change the namespace

--- src/main/java/com/example/authbe/controller/AuthenticationController.java
+++ src/main/java/com/example/authbe/controller/AuthenticationController.java
@@ -12,7 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthService authService;
