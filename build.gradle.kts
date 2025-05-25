0e0eb5b04b08392113edb6bb51d5cdbe46b68421
TrayMachi <tristan.agra@ui.ac.id>
2025-05-25 18:02:19 +0700

    build: monitoring dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-rest")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("io.micrometer:micrometer-registry-prometheus")
 	implementation("jakarta.validation:jakarta.validation-api:3.0.2")
 	implementation("org.hibernate.validator:hibernate-validator:8.0.1.Final")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
